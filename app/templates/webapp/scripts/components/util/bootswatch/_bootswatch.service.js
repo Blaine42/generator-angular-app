12d0534fda49b2145f86f37a1bf8aebc0d7ddafd
Romain <rbeaulieu@adista.fr>
2016-03-04 10:45:32 +0100

    change bootswatch api url

--- app/templates/webapp/scripts/components/util/bootswatch/_bootswatch.service.js
+++ app/templates/webapp/scripts/components/util/bootswatch/_bootswatch.service.js
@@ -4,7 +4,7 @@ angular.module('<%=angularAppName%>')
     .factory('BootSwatchService', function ($http) {
         return {
             get: function() {
-                return $http.get('http://api.bootswatch.com/3/').then(function (response) {
+                return $http.get('https://bootswatch.com/api/3.json').then(function (response) {
                     return response.data.themes;
                 });
             }
